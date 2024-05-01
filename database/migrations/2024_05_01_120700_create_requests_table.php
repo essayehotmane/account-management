652045271c83f445ffcede31bbc552eb8a5b2ac1
essayehotmane <57506688+essayehotmane@users.noreply.github.com>
2024-05-01 13:46:31 +0100

    Setup project with migrations

---
+++ database/migrations/2024_05_01_120700_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->boolean('email_sent')->default(0);
+            $table->timestamp('email_sent_at')->nullable(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
