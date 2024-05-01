45b35b73fc09c205393c871f560a26a4a0994e49
essayehotmane <57506688+essayehotmane@users.noreply.github.com>
2024-05-01 14:12:17 +0100

    Fix name

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Account
+ * Class Role
  * 
  * @property int id
  * @property string name
