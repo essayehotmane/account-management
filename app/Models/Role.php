1f291476b2c6da78685cc65cd682af9f68c26293
essayehotmane <57506688+essayehotmane@users.noreply.github.com>
2024-05-01 14:58:16 +0100

    Add relation between Role And User

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,14 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class Role
- * 
+ *
  * @property int id
  * @property string name
  */
 class Role extends Model
 {
     use HasFactory;
+
+    const ADMIN_ID  = 1;
+    const CLIENT_ID = 2;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Get the users.
+     *
+     * @return HasMany
+     */
+    public function role(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
