864a1bc09f28db7ed9d20e4122bd48ecee3249ac
essayehotmane <57506688+essayehotmane@users.noreply.github.com>
2024-05-04 01:45:41 +0100

    Add button to export users in csv file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Response;
 
 class UserController extends Controller
 {
@@ -28,5 +29,43 @@ class UserController extends Controller
             ->paginate(50);
 
         return view('Users', ['users' => $users]);
+    }
+
+    /**
+     * Export users csv.
+     */
+    public function exportCsv()
+    {
+        $users = User::query()
+            ->where('role_id', Role::CLIENT_ID)
+            ->get();
+
+        $csvFileName = 'users.csv';
+
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => "attachment; filename=$csvFileName",
+        ];
+
+        $callback = function () use ($users) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, ['ID', 'Name', 'Email', 'Phone', 'Device', 'App', 'Subscribed at']);
+
+            foreach ($users as $user) {
+                fputcsv($file, [
+                    $user->id,
+                    $user->name,
+                    $user->email,
+                    $user->phone,
+                    $user->device,
+                    $user->app,
+                    $user->created_at,
+                ]);
+            }
+
+            fclose($file);
+        };
+
+        return Response::stream($callback, 200, $headers);
     }
 }
